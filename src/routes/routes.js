3d76c0a17ca7a8dc020d8e77db6df75baad8a7c4
Brian Villanova <brian.rj99@yahoo.com.br>
2021-11-19 12:21:07 +0000

    adding model and controller of products

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,24 @@
 const router = require("express").Router()
 
-router.get('/clientes', (req, res) => {
-    res.send({
-        ok: 123,
-    })
-})
+const ProductController = require('../controllers/products')
+
+router.get('/products', ProductController.get)
+//router.post('/products', ProductController.post)
+//router.put('/products/:id', ProductController.put)
+//router.delete('/products/:id', ProductController.delete)
+
+/* Verbos HTTP (4 tipos)
+
+GET - OBTER DADOS
+POST - ENVIAR DADOS
+PUT - ATUALIZAR DADOS
+DELETE - DELETAR DADOS
+
+CRUD - CREATE READ UPDATE AND DELETE
+
+
+
+
+*/
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 const express = require("express")
 //const path = require("path")
 
-//const db = require ("./database")
+const db = require ("./database/db")
 const routes = require('./routes/routes')
 
 const app = express()
 
 //conexão com database
-
-//db.connect()
+db.connect()
 
 //habilita server para receber dados via post(formulário)
 app.use(express.urlencoded({extended: true}))
